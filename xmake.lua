c8ecddf49984eefea82b89a0d5827e2283ddcfd5
JaJaLys <sylvain.janniere@laposte.net>
2026-03-16 22:14:28 +0100

    fix xmake integration

--- xmake.lua
+++ xmake.lua
@@ -3,7 +3,7 @@ set_version("1.x")
 
 target("tinyxml-boosted")
     set_kind("static")
-
     add_files("$(projectdir)/Sources/**.cpp")
     add_headerfiles("$(projectdir)/Include/TinyXML_Boosted/**.h", "$(projectdir)/Include/TinyXML_Boosted/**.hpp")
     add_includedirs("$(projectdir)/Include/", {public = true})
+    add_includedirs("$(projectdir)/Include/TinyXML_Boosted/")
