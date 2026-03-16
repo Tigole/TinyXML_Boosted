9dc91a9620ed3be8ec267539600385d51c9a2272
JaJaLys <sylvain.janniere@laposte.net>
2026-03-16 13:57:55 +0100

    xmake support

---
+++ Projects/xmake/xmake.lua
@@ -0,0 +1,9 @@
+set_project("TinyXML_Boosted")
+set_version("1.x")
+
+target("tinyxml_boosted")
+    set_kind("static")
+
+    add_files("$(projectdir)/Sources/**.cpp")
+    add_headerfiles("$(projectdir)/Include/TinyXML_Boosted/**.h", "$(projectdir)/Include/TinyXML_Boosted/**.hpp")
+    add_includedirs("$(projectdir)/Include/", {public = true})
